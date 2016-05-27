21d4313c96a2227806d15c5011a65954d0ff24f8
1 <1>
2016-05-27 14:24:03 +0800

    22

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #根据时间生成版本号
-TAG=$JOB_NAME:`date +%y%m%d-%H-%M`
+REG_URL='web3:5000'
+TAG=$REG_URL/$JOB_NAME:`date +%y%m%d-%H-%M`
 #使用maven 镜像进行编译 打包出 war 文件 （其他语言这里换成其他编译镜像）
 docker run --rm --name mvn  -v /mnt/maven:/root/.m2 -v /mnt/jenkins_home/workspace/$JOB_NAME:/usr/src/mvn -w /usr/src/mvn/ maven:3.3.3-jdk-8 mvn clean install -Dmaven.test.skip=true
 #使用我们刚才写好的 放在项目下面的Dockerfile 文件打包 
